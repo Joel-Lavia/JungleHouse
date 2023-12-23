34e432f4ced9d1a44204c904b662d87158e446b4
Joel-Lavia <joeldisidi8@gmail.com>
2023-12-23 20:25:57 +0100

    init projet

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,9 @@
+import Logo from '../assets/Icons/logo.png'
+function Banner() {
+ return(
+    <div>
+        <h1> <img src={Logo} alt="Logo jungle house" /> Jungle House</h1>
+    </div>
+ )   
+}
+export default Banner;
